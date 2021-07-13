4110c5d18a26315ab4c62cbcb3e980c4d46d5726
傲娇大魔兽 <605775776@qq.com>
2021-07-13 18:46:45 +0800

    order

--- src/main/java/com/imooc/mall/controller/OrderController.java
+++ src/main/java/com/imooc/mall/controller/OrderController.java
@@ -1,6 +1,85 @@
-package com.imooc.mall.controller;/**
-  *@Description      
-  *@author          dsw
-  *@create          2021-07-13 12:44
-  */  public class OrderController {
+package com.imooc.mall.controller;
+
+import com.imooc.mall.model.vo.CartVO;
+import com.imooc.mall.service.CartService;
+import com.imooc.mall.service.OrderService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author dsw
+ * @Description
+ * @create 2021-07-13 12:44
+ */
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    CartService cartService;
+
+
+    @PostMapping("/create")
+    @ApiOperation("创建订单")
+    public void createOrder(@RequestParam String receiverName, @RequestParam String receiverMobile, @RequestParam String receiverAddress){
+
+    }
+
+    @PostMapping("/detail")
+    @ApiOperation("订单详情")
+    public void orderDetail(@RequestParam Integer orderNo){
+
+    }
+
+    @PostMapping("/list")
+    @ApiOperation("订单列表")
+    public List<> orderList(@RequestParam Integer pageNum, @RequestParam Integer pageSize){
+
+    }
+
+
+    @PostMapping("/cancel")
+    @ApiOperation("取消订单")
+    public void cancelOrder(@RequestParam Integer orderNo){
+
+    }
+
+    @PostMapping("/qrcode")
+    @ApiOperation("取消订单")
+    public void createQrcode(@RequestParam Integer orderNo){
+
+    }
+
+    @GetMapping("/pay")
+    @ApiOperation("支付订单")
+    public void payOrder(@RequestParam Integer orderNo){
+
+    }
+
+    @PostMapping("/admin/order/list")
+    @ApiOperation("后台订单列表")
+    public void adminList(@RequestParam Integer pageNum, @RequestParam Integer pageSize){
+
+    }
+
+
+    @PostMapping("/admin/order/delivered")
+    @ApiOperation("订单发货")
+    public void orderDelivered(@RequestParam Integer orderNo){
+
+    }
+
+    @PostMapping("/finish")
+    @ApiOperation("订单完结")
+    public void orderFinish(@RequestParam Integer orderNo){
+
+    }
+
+
 }
--- src/main/java/com/imooc/mall/service/impl/OrderServiceImpl.java
+++ src/main/java/com/imooc/mall/service/impl/OrderServiceImpl.java
@@ -1,6 +1,19 @@
-package com.imooc.mall.service.impl;/**
-  *@Description      
-  *@author          dsw
-  *@create          2021-07-13 12:47
-  */  public class OrderServiceImpl {
+package com.imooc.mall.service.impl;
+
+import com.imooc.mall.model.dao.OrderMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author dsw
+ * @Description
+ * @create 2021-07-13 12:47
+ */
+@Service
+public class OrderServiceImpl {
+
+    @Autowired
+    OrderMapper orderMapper;
+
+
 }
